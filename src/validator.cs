f95024ea4f8ae5970efc5045ff4a616f198f351b
Jakub Čábera <cabera.jakub@gmail.com>
2021-06-15 19:47:05 +0200

    chore: Reformat code as K&R (#2)

--- src/validator.cs
+++ src/validator.cs
@@ -4,17 +4,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace Scoop
-{
-    public class Program
-    {
-        public static int Main(string[] args)
-        {
+namespace Scoop {
+    public class Program {
+        public static int Main(string[] args) {
             bool ci = String.Format("{0}", Environment.GetEnvironmentVariable("CI")).ToLower() == "true";
             bool valid = true;
 
-            if (args.Length < 2)
-            {
+            if (args.Length < 2) {
                 Console.WriteLine("Usage: validator.exe <schema> <manifest> [<manifest>...]");
                 return 1;
             }
@@ -24,7 +20,7 @@ namespace Scoop
             String schema = manifests.First();
             manifests.RemoveAt(0);
             String combinedArgs = String.Join("", manifests);
-            if(combinedArgs.Contains("*") || combinedArgs.Contains("?")) {
+            if (combinedArgs.Contains("*") || combinedArgs.Contains("?")) {
                 try {
                     var path = new Uri(Path.Combine(Directory.GetCurrentDirectory(), combinedArgs)).LocalPath;
                     var drive = Path.GetPathRoot(path);
@@ -37,25 +33,21 @@ namespace Scoop
             }
 
             Scoop.Validator validator = new Scoop.Validator(schema, ci);
-            foreach(var manifest in manifests) {
-                if (validator.Validate(manifest))
-                {
-                    if(ci) {
+            foreach (var manifest in manifests) {
+                if (validator.Validate(manifest)) {
+                    if (ci) {
                         Console.WriteLine("      [+] {0} validates against the schema!", Path.GetFileName(manifest));
                     } else {
                         Console.WriteLine("- {0} validates against the schema!", Path.GetFileName(manifest));
                     }
-                }
-                else
-                {
-                    if(ci) {
+                } else {
+                    if (ci) {
                         Console.WriteLine("      [-] {0} has {1} Error{2}!", Path.GetFileName(manifest), validator.Errors.Count, validator.Errors.Count > 1 ? "s" : "");
                     } else {
                         Console.WriteLine("- {0} has {1} Error{2}!", Path.GetFileName(manifest), validator.Errors.Count, validator.Errors.Count > 1 ? "s" : "");
                     }
                     valid = false;
-                    foreach (var error in validator.Errors)
-                    {
+                    foreach (var error in validator.Errors) {
                         Console.WriteLine(error);
                     }
                 }
